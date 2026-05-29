69ee40075275e16d3fdaaa7d14a28347dd9a2ea5
Liniker Isquierdo <56650677+LinikerLopes@users.noreply.github.com>
2026-05-29 09:04:30 -0300

    Add initial implementation of BoletimAlunos.c

---
+++ Exercicios dados em Aula/BoletimAlunos.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+typedef struct {
+char nome[50];
+char curso[50];
+float notas[3];
+float media;
+} Aluno;
+
+int main(){
+
+  Aluno aluno[5];
+  
+  int n = sizeof(aluno) / sizeof(aluno[0]);
+  
+  for(int i = 0; i < n; i++){
+    
+  }
+
+  return 0;
+}
